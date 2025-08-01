45e2173905970952be2129615b2b36f29f9303d6
caffeinetrip <shizenking@gmail.com>
2025-08-01 11:52:42 +0300

    add: asset_utils

---
+++ rest/assets/asset_utils.py
@@ -0,0 +1,19 @@
+import pygame
+
+from rest.utils.io import recursive_file_op
+
+def load_img(path: str, alpha=False, colorkey=None):
+    
+    if alpha:
+        img = pygame.image.load(path).convert_alpha()
+        
+    else:
+        pygame.image.load(path).convert()
+        
+    if colorkey:
+        img.set_colorkey(colorkey)
+        
+    return img
+
+def load_img_directory(path, alpha=False, colorkey=None):
+    return recursive_file_op(path, lambda x: load_img(x, alpha=alpha, colorkey=colorkey), filetype='png')
---
+++ rest/assets/assets.py
@@ -0,0 +1,3 @@
+from rest.utils.io import read_json
+from .spritesheets import load_spritesheets
+from .asset_utils import load_img_directory
